6116442f2151924e60a8b331dd4f7da140a842fd
Fuaad <fuaadmohamoud@gmail.com>
2025-06-06 14:39:28 -0500

    Implement MedianFinder using two heaps for O(log n) insertion
    - Use max heap for smaller half and min heap for larger half
    - Maintain balanced heaps with at most 1 element difference
    - Support O(log n) addNum() and O(1) findMedian() operations

--- medianFinder.py
+++ medianFinder.py
@@ -3,48 +3,27 @@ import heapq
 class MedianFinder:
 
     def __init__(self):
-        self.small = [] #max Heap largest value in 0
-        self.large = [] #min Heap smallest value in 0
-        
+        self.small = []  # Max heap (store negatives for largest value on top)
+        self.large = []  # Min heap (store as-is for smallest value on top)
 
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.small,-num)
+        # Add to max heap (simulate by pushing negative)
+        heapq.heappush(self.small, -num)
 
-        if (self.small and self.large) and ((self.small[0]  * - 1) > self.large[0]):
-            val = -1 * heapq.heappop(self.small)
-            heapq.heappush(self.large,val)
+        # Ensure ordering: small's max should be <= large's min
+        if self.small and self.large and (-self.small[0] > self.large[0]):
+            heapq.heappush(self.large, -heapq.heappop(self.small))
 
-        if len(self.small) > len(self.large):
-            val = -1 * heapq.heappop(self.small)
-            heapq.heappush(self.large,val)
-
+        # Balance the heaps
+        if len(self.small) > len(self.large) + 1:
+            heapq.heappush(self.large, -heapq.heappop(self.small))
         elif len(self.large) > len(self.small):
-            val = -1 * heapq.heappop(self.large)
-            heapq.heappush(self.small,val)
-
-        
+            heapq.heappush(self.small, -heapq.heappop(self.large))
 
     def findMedian(self) -> float:
         if len(self.small) > len(self.large):
-            return -1 * self.small[0]
+            return -self.small[0]
         if len(self.large) > len(self.small):
             return self.large[0]
-
-        return ((-1 * self.small[0]) + self.large[0])/2
-        
-
-
-if __name__ == "__main__":
-    obj = MedianFinder()
-    obj.addNum(1)
-    obj.addNum(2)
-    print(obj.findMedian())
-    obj.addNum(3)
-    print(obj.findMedian())
-
-
-
-# Your MedianFinder object will be instantiated and called as such:
-# obj = MedianFinder()
-# obj.addNum(num)
-# param_2 = obj.findMedian()
+        # Even number of elements
+        return (-self.small[0] + self.large[0]) / 2
